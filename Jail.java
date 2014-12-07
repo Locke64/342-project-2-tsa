3c61f48b1ca4f35912931dca931e10a74932bbf6
AcidHotpocket <rej7869@rit.edu>
2014-12-07 10:16:12 -0500

    Jail has been updated to include functionality and should be reviewed.

--- Jail.java
+++ Jail.java
@@ -8,6 +8,14 @@ public class Jail extends UntypedActor {
 	public void onReceive( Object message ) {
 		if( message instanceof FailedPassenger ) {
 			//TODO
+			System.out.println("Looks like another passenger is going to JAIL!"
+				+ "(Jail receives FailedPassenger message)");
+			FailedPassenger passenger = (FailedPassenger) message;
+			passengers.add(passenger);
+			System.out.println("They're in the jailhouse now!(FailedPassenger added to Jail)");
+		}else if (message instanceof PoisonPill) {
+			//TODO this section is merely conjecture. Will consider other options later.
+			System.out.printf("Today, %d criminals are being sent to the bighouse!\n", passengers.size()); 
 		} else {
 			unhandled( message );
 		}
